7571adf42be1be6d97b001f225a44c2f7d23ffb7
Nick <nickdbailey20@gmail.com>
2021-08-03 13:58:21 -0500

    added terraform apply and automated webhook url updated to setup script

--- setup.sh
+++ setup.sh
@@ -9,5 +9,15 @@ cd ../tmf-reception-lambda
 7z a tmf-reception-lambda.zip *.py
 cd ../..
 
-#terraform init
-#terraform plan
+terraform init
+terraform plan
+terraform apply -auto-approve
+
+account=$(terraform output -raw account_sid)
+phone=$(terraform output -raw number_sid)
+url=$(terraform output -raw tmf-invoke-url)
+token=$(terraform output -raw token)
+
+curl -XPOST https://api.twilio.com/2010-04-01/Accounts/$account/IncomingPhoneNumbers/$phone.json \
+    --data-urlencode "SmsUrl=$url" \
+    -u "$account:$token"
