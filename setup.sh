158519053e2985d8686952069490c37ffcce7276
Nick <nickdbailey20@gmail.com>
2021-08-05 16:46:54 -0500

    adding some informative messages to setup script

--- setup.sh
+++ setup.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+echo "Building Lambda zip files from sources..."
 cd lambda/tmf-app-lambda/libraries
-7z a -r ../tmf-app-lambda.zip .
+7z a -r ../tmf-app-lambda.zip .  > /dev/null 2>&1
 cd ..
 7z a tmf-app-lambda.zip *.py
 
 cd ../tmf-reception-lambda
-7z a tmf-reception-lambda.zip *.py
+7z a tmf-reception-lambda.zip *.py  > /dev/null 2>&1
 cd ../..
 
-terraform init
-terraform plan
+echo "Initializing and building application..."
+terraform init  > /dev/null 2>&1
+terraform plan  > /dev/null 2>&1
 terraform apply -auto-approve
 
 account=$(terraform output -raw account_sid)
@@ -18,6 +20,14 @@ phone=$(terraform output -raw number_sid)
 url=$(terraform output -raw tmf-invoke-url)
 token=$(terraform output -raw token)
 
-curl -XPOST https://api.twilio.com/2010-04-01/Accounts/$account/IncomingPhoneNumbers/$phone.json \
+echo "Integrating Twilio phone number messaging with AWS deployment..."
+curl -s -XPOST https://api.twilio.com/2010-04-01/Accounts/$account/IncomingPhoneNumbers/$phone.json \
     --data-urlencode "SmsUrl=$url" \
     -u "$account:$token"
+
+if [ $? -eq 0 ]
+then
+    echo "TMF application successfully deployed, text and say hello!"
+else
+    echo "Uh oh, something went wrong with TMF build..."
+fi
