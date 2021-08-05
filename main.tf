813e91163367be9913ac61b89e51176b15deb695
Nick <nickdbailey20@gmail.com>
2021-08-05 17:57:20 -0500

    added terraform trigger to api gw deployment resource

--- main.tf
+++ main.tf
@@ -628,11 +628,28 @@ EOF
 
 
 resource "aws_api_gateway_deployment" "tmfApiDeployment" {
-  depends_on  = [
-    aws_api_gateway_method.tmfApiMethod,
-    aws_api_gateway_integration.tmfApiIntegration
-  ]
   rest_api_id = aws_api_gateway_rest_api.tmfApi.id
+
+  triggers = {
+    # NOTE: The configuration below will satisfy ordering considerations,
+    #       but not pick up all future REST API changes. More advanced patterns
+    #       are possible, such as using the filesha1() function against the
+    #       Terraform configuration file(s) or removing the .id references to
+    #       calculate a hash against whole resources. Be aware that using whole
+    #       resources will show a difference after the initial implementation.
+    #       It will stabilize to only change when resources change afterwards.
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.tmfApiResource,
+      aws_api_gateway_method.tmfApiMethod,
+      aws_api_gateway_integration.tmfApiIntegration,
+      aws_api_gateway_method_response.tmfSuccessMethodResponse,
+      aws_api_gateway_integration_response.tmfSuccessIntegrationResponse
+    ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_api_gateway_stage" "tmfApiStage" {
