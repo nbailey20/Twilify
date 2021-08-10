bcb662ca3a3fd0c8916335c04ed6c6c0b206699d
Nick <nickdbailey20@gmail.com>
2021-08-10 01:35:23 -0500

    works with reset or no keyword only, ready to be done with slow network stack

--- lambda/tmf-reception-lambda/textHandler.py
+++ lambda/tmf-reception-lambda/textHandler.py
@@ -11,15 +11,15 @@ def parse_text(DEBUG, body):
         playlist_params["reset"] = True
 
     ## check to see if size keyword included in text
-    match = re.search(r"size [0-9]+", body)
+    match = re.search(r"size\+[0-9]+", body)
     if match is not None:
-        if DEBUG: print("DEBUG: found size keyword")
+        if DEBUG: print("DEBUG: found size keyword", match.group())
         playlist_params["size"] = int(match.group().split(" ")[1])
 
     ## check to see if keyword keyword included in text
-    match = re.search(r"keyword \w+", body)
+    match = re.search(r"keyword\+\w+", body)
     if match is not None:
-        if DEBUG: print("DEBUG: found keyword keyword")
+        if DEBUG: print("DEBUG: found keyword keyword", match.group())
         playlist_params["keyword"] = match.group().split(" ")[1]
     
     return playlist_params
