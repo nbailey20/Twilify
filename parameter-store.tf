38231ab9b15ce8dc0730f0c53c984b606c423d7f
Nick <nickdbailey20@gmail.com>
2021-08-09 23:59:38 -0500

    encrypt parameter store again cuz visual studio slow

--- parameter-store.tf
+++ parameter-store.tf
@@ -8,14 +8,16 @@ resource "aws_ssm_parameter" "spotify_refresh_token" {
 
 resource "aws_ssm_parameter" "playlist_text_params" {
   name      = "/tmf/playlist_text_params"
-  type      = "String"
-  value     = "{reset: False, size: ${var.num_songs_in_playlist}, keyword: None}"
+  type      = "SecureString"
+  value     = "{}"
+  key_id    = aws_kms_key.tmf_kms_key.arn
   overwrite = false
 }
 
 resource "aws_ssm_parameter" "network_keep_alive" {
   name      = "/tmf/network_keep_alive"
-  type      = "String"
+  type      = "SecureString"
   value     = "0"
+  key_id    = aws_kms_key.tmf_kms_key.arn
   overwrite = false
 }
